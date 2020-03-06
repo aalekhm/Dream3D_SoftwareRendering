8c4548bf88a4e06e751b738666ed5c1502ec9b02
aalekhm <aalekhm@gmail.com>
2020-03-07 01:39:25 +0530

    _16_SoftwareRenderer_Clipping
    Run in Release mode.

---
+++ _16_SoftwareRenderer_Clipping/include/Vector4f.h
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+
+class Vector4f
+{
+	public:
+						Vector4f();
+						Vector4f(float x, float y, float z, float w);
+		float			length();
+		float			max_();
+		float			dot(Vector4f r);
+		Vector4f		cross(Vector4f r);
+		Vector4f		normalized();
+		Vector4f		rotate(Vector4f axis, float angle);
+		//Vector3f		rotate(Quaternion rotation);
+		Vector4f		lerp(Vector4f dest, float lerpFactor);
+		Vector4f		add(Vector4f r);
+		Vector4f		add(float r);
+		Vector4f		sub(Vector4f r);
+		Vector4f		sub(float r);
+		Vector4f		mul(Vector4f r);
+		Vector4f		mul(float r);
+		Vector4f		div(Vector4f r);
+		Vector4f		div(float r);
+		Vector4f		abs_();
+		std::string		toString();
+		float			getX();
+		float			getY();
+		float			getZ();
+		float			getW();
+		bool			equals(Vector4f r);
+
+		float			x;
+		float			y;
+		float			z;
+		float			w;
+	protected:
+	private:
+};
---
+++ _16_SoftwareRenderer_Clipping/src/Edge.cpp
@@ -0,0 +1,82 @@
+#include "Edge.h"
+
+Edge::Edge(Gradients pGradients, Vertex vStart, Vertex vEnd, int32_t iMinYVertIndex)
+{
+	m_iYStart = (int32_t)ceil(vStart.GetY());
+	m_iYEnd = (int32_t)ceil(vEnd.GetY());
+
+	float fXDist = vEnd.GetX() - vStart.GetX();
+	float fYDist = vEnd.GetY() - vStart.GetY();
+
+	float fYPreStep = m_iYStart - vStart.GetY();
+
+	m_fXStep = (float)fXDist / (float)fYDist;
+	m_fX = vStart.GetX() + (fYPreStep * m_fXStep);
+
+	float fXPreStep = m_fX - vStart.GetX();
+
+	m_fTexCoordX 	= 	pGradients.GetTexCoordX(iMinYVertIndex) +
+						pGradients.GetTexCoordXXStep() * fXPreStep +
+						pGradients.GetTexCoordXYStep() * fYPreStep;
+	m_fTexCoordXStep = 	pGradients.GetTexCoordXYStep() + pGradients.GetTexCoordXXStep() * m_fXStep;
+
+	m_fTexCoordY 	= 	pGradients.GetTexCoordY(iMinYVertIndex) +
+						pGradients.GetTexCoordYXStep() * fXPreStep +
+						pGradients.GetTexCoordYYStep() * fYPreStep;
+	m_fTexCoordYStep = 	pGradients.GetTexCoordYYStep() + pGradients.GetTexCoordYXStep() * m_fXStep;
+
+	m_fOneOverZ 	= 	pGradients.GetOneOverZ(iMinYVertIndex) +
+						pGradients.GetOneOverZXStep() * fXPreStep +
+						pGradients.GetOneOverZYStep() * fYPreStep;
+	m_fOneOverZStep = 	pGradients.GetOneOverZYStep() + pGradients.GetOneOverZXStep() * m_fXStep;
+
+	m_fDepth 		= 	pGradients.GetDepth(iMinYVertIndex) +
+						pGradients.GetDepthXStep() * fXPreStep +
+						pGradients.GetDepthYStep() * fYPreStep;
+	m_fDepthStep	= 	pGradients.GetDepthYStep() + pGradients.GetDepthXStep() * m_fXStep;
+}
+
+
+int32_t Edge::GetYStart()
+{
+	return m_iYStart;
+}
+
+int32_t Edge::GetYEnd()
+{
+	return m_iYEnd;
+}
+
+float Edge::GetX()
+{
+	return m_fX;
+}
+
+float Edge::GetTexCoordX()
+{
+	return m_fTexCoordX;
+}
+
+float Edge::GetTexCoordY()
+{
+	return m_fTexCoordY;
+}
+
+float Edge::GetOneOverZ()
+{
+	return m_fOneOverZ;
+}
+
+float Edge::GetDepth()
+{
+	return m_fDepth;
+}
+
+void Edge::Step()
+{
+	m_fX			+= m_fXStep;
+	m_fTexCoordX	+= m_fTexCoordXStep;
+	m_fTexCoordY	+= m_fTexCoordYStep;
+	m_fOneOverZ		+= m_fOneOverZStep;
+	m_fDepth		+= m_fDepthStep;
+}
